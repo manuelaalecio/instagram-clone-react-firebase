b944b2bc79e4f688f43205955a64ce1c67819378
manuela-alecio <manuelaalecio.dev@gmail.com>
2023-12-07 12:38:20 -0300

    Responsive instagram logo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,44 @@
+import { Box, Flex, Link } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import { InstagramLogo, InstagramMobileLogo } from '../../assets/contants';
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <Box
+      height={'100vh'}
+      borderRight={'1px solid'}
+      borderColor={'whiteAlpha.300'}
+      py={8}
+      position={'sticky'}
+      top={0}
+      left={0}
+      px={{ base: 2, md: 4 }}
+    >
+      <Flex direction={'column'} gap={10} width={'full'} height={'full'}>
+        <Link
+          to={'/'}
+          as={RouterLink}
+          pl={2}
+          display={{ base: 'none', md: 'block' }}
+          cursor={'pointer'}
+        >
+          <InstagramLogo />
+        </Link>
+        <Link
+          to={'/'}
+          as={RouterLink}
+          pl={2}
+          display={{ base: 'block', md: 'none' }}
+          borderRadius={6}
+          _hover={{ bg: 'whiteAlpha.200' }}
+          w={10}
+          cursor={'pointer'}
+        >
+          <InstagramMobileLogo />
+        </Link>
+      </Flex>
+    </Box>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
