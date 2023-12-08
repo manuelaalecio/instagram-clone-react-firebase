161b385effd11151f76048beff222310fbda0669
manuela-alecio <manuelaalecio.dev@gmail.com>
2023-12-08 16:09:57 -0300

    add Suggested Header

---
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -0,0 +1,20 @@
+import { Avatar, Flex, Link, Text } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+
+const SuggestedHeader = () => {
+  return (
+    <Flex justifyContent={'space-between'} alignItems={'center'} w={'full'}>
+      <Flex alignItems={'center'} gap={2}>
+        <Avatar name="manu" size={'sm'} src="/profilepic.jpg" />
+        <Text fontSize={12} fontWeight={'bold'}>
+          manu
+        </Text>
+      </Flex>
+      <Link as={RouterLink} to={'/auth'} fontSize={14} fontWeight={'medium'} color={'blue.400'} style={{textDecoration: 'none'}} cursor={'pointer'}>
+        Log out
+      </Link>
+    </Flex>
+  );
+};
+
+export default SuggestedHeader;
