79e47a4c81e759f704c3b28e8a3aaccfb3f9eb5e
manuela-alecio <manuelaalecio.dev@gmail.com>
2023-12-08 10:54:02 -0300

    structuring post

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -14,7 +14,7 @@ import {
   UnlikeLogo,
 } from '../../assets/constants';
 
-const PostFooter = () => {
+const PostFooter = ({username}) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
 
@@ -43,7 +43,7 @@ const PostFooter = () => {
         {likes} likes
       </Text>
       <Text fontWeight={700} fontSize={'sm'}>
-        manu{' '}
+        {username}{' '}
         <Text as={'span'} fontWeight={400}>
           Feeling good
         </Text>
--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,6 +1,6 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-const PostHeader = () => {
+const PostHeader = ({ username, avatar }) => {
   return (
     <Flex
       justifyContent={'space-between'}
@@ -9,9 +9,9 @@ const PostHeader = () => {
       mb={2}
     >
       <Flex alignItems={'center'} gap={2}>
-        <Avatar src="/img1.png" alt="user profile picture" size={'sm'} />
+        <Avatar src={avatar} alt="user profile picture" size={'sm'} />
         <Flex fontSize={12} fontWeight={'bold'} gap={2}>
-          username
+          {username}
           <Box color={'gray.500'}>• 1w</Box>
         </Flex>
       </Flex>
