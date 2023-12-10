aca649f4c6ae0a78754ff85426a85c4cc80b0b72
manuela-alecio <manuelaalecio.dev@gmail.com>
2023-12-10 16:36:29 -0300

    Add suggested user, avatar, name, followers

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,7 +1,37 @@
-const SuggestedUser = () => {
-  return (
-    <div>SuggestedUser</div>
-  )
-}
+import { Avatar, Button, Flex, Text, VStack } from '@chakra-ui/react';
+import { useState } from 'react';
 
-export default SuggestedUser
+const SuggestedUser = ({ name, followers, avatar }) => {
+  const [isFollowed, setIsFollowed] = useState(false);
+
+  return (
+    <Flex justifyContent={'space-between'} alignItems={'center'} w={'full'}>
+      <Flex alignItems={'center'} gap={2}>
+        <Avatar src={avatar} name={name} size={'md'} />
+        <VStack spacing={2} alignItems={'flex-start'}>
+          <Text fontSize={12} fontWeight={'bold'}>
+            {name}
+          </Text>
+          <Text fontSize={11} color={'gray.500'}>
+            {followers} followers
+          </Text>
+        </VStack>
+      </Flex>
+      <Button
+        fontSize={13}
+        bg={'transparent'}
+        p={0}
+        h={'max-content'}
+        fontWeight={'medium'}
+        color={'blue.400'}
+        cursor={'pointer'}
+        _hover={{ color: 'white' }}
+        onClick={() => setIsFollowed(!isFollowed)}
+      >
+        {isFollowed ? 'Unfollow' : 'Follow'}
+      </Button>
+    </Flex>
+  );
+};
+
+export default SuggestedUser;
--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,12 +1,34 @@
-import { VStack } from "@chakra-ui/react"
-import SuggestedHeader from "./SuggestedHeader"
+import { Box, Flex, Link, Text, VStack } from '@chakra-ui/react';
+import SuggestedHeader from './SuggestedHeader';
+import SuggestedUser from './SuggestedUser';
 
 const SuggestedUsers = () => {
   return (
     <VStack py={8} px={8} gap={4}>
       <SuggestedHeader />
-    </VStack>
-  )
-}
 
-export default SuggestedUsers
+      <Flex alignItems={'center'} justifyContent={'space-between'} w={'full'}>
+        <Text fontSize={12} fontWeight={'bold'} color={'gray.500'}>
+          Suggested for you
+        </Text>
+        <Text
+          fontSize={12}
+          fontWeight={'bold'}
+          _hover={{ color: 'gray.400' }}
+          cursor={'pointer'}
+        >
+          See All
+        </Text>
+      </Flex>
+      <SuggestedUser name='Dan Abrahmov' followers={1392} avatar='https://bit.ly/dan-abramov' />
+      <SuggestedUser name='Ryan Florence' followers={567} avatar='https://bit.ly/ryan-florence' />
+      <SuggestedUser name='Christian Nwamba' followers={759} avatar='https://bit.ly/code-breast' />
+
+      <Box fontSize={12} color={'gray.500'} mt={5}>
+        © 2023 build By <Link href="https://github.com/manuelaalecio" target='_blank' color={'gray.500'} fontSize={14}>Manuela Alecio</Link>
+      </Box>
+    </VStack>
+  );
+};
+
+export default SuggestedUsers;
